55ffa9e4d8abe2aa562088aa403bb539cac9aa54
Max Malynowsky <github@maxmalynowsky.com>
2023-12-10 23:02:02 +0100

    add API to SvelteKit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 // import { CONNECT_STR, CONTAINER } from '$env/static/private';
 // import { BlobServiceClient } from '@azure/storage-blob';
-// import { error } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
 // const blobServiceClient = CONNECT_STR ? BlobServiceClient.fromConnectionString(CONNECT_STR) : null;
 // const containerClient = CONTAINER ? blobServiceClient?.getContainerClient(CONTAINER) : null;
@@ -27,8 +27,8 @@ function base64ToJSON(header: string) {
 }
 
 export function authorize(headers: Headers, slug: string) {
-  const authorization = headers.get('authorization');
-  const userRoles = authorization ? base64ToJSON(authorization.split(' ')[1]).userRoles : [];
-  // const role = slug.replace('-', '_');
-  // if (!userRoles.includes(role)) throw error(401, 'Not authorized to access this resource');
+  const authorization = headers.get('x-ms-client-principal');
+  const userRoles = authorization ? base64ToJSON(authorization).userRoles : [];
+  const role = slug.replace('-', '_');
+  if (!userRoles.includes(role)) throw error(401, 'Not authorized to access this resource');
 }
