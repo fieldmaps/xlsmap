87fbcf4e2cb7a9e2775073e34b1f1e1264d2b4bb
Max Malynowsky <github@maxmalynowsky.com>
2023-12-10 18:18:53 +0100

    add API to SvelteKit

--- src/routes/api/utils.ts
+++ src/routes/api/utils.ts
@@ -1,19 +1,23 @@
-import { CLIENT, CONNECT_STR } from '$env/static/private';
+import { CONNECT_STR, CONTAINER } from '$env/static/private';
 import { BlobServiceClient } from '@azure/storage-blob';
 import { error } from '@sveltejs/kit';
 
-const blobServiceClient = BlobServiceClient.fromConnectionString(CONNECT_STR);
-const containerClient = blobServiceClient.getContainerClient(CLIENT);
+const blobServiceClient = CONNECT_STR ? BlobServiceClient.fromConnectionString(CONNECT_STR) : null;
+const containerClient = CONTAINER ? blobServiceClient?.getContainerClient(CONTAINER) : null;
 
 export async function readFile(blobName: string) {
-  const blobClient = containerClient.getBlockBlobClient(blobName);
-  const blobResponse = await blobClient.download();
-  return blobResponse.readableStreamBody;
+  if (containerClient) {
+    const blobClient = containerClient.getBlockBlobClient(blobName);
+    const blobResponse = await blobClient.download();
+    return blobResponse.readableStreamBody;
+  }
 }
 
 export async function updateFile(blobName: string, buffer: ArrayBuffer, contentType: string) {
-  const blobClient = containerClient.getBlockBlobClient(blobName);
-  await blobClient.uploadData(buffer, { blobHTTPHeaders: { blobContentType: contentType } });
+  if (containerClient) {
+    const blobClient = containerClient.getBlockBlobClient(blobName);
+    await blobClient.uploadData(buffer, { blobHTTPHeaders: { blobContentType: contentType } });
+  }
 }
 
 export function authorize(headers: Headers, slug: string) {
