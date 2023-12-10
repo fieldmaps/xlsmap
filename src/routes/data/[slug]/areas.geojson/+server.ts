697336ca37807c342f3afaaa1ed41b55cdf67063
Max Malynowsky <github@maxmalynowsky.com>
2023-12-10 22:20:42 +0100

    add API to SvelteKit

--- src/routes/data/[slug]/areas.geojson/+server.ts
+++ src/routes/data/[slug]/areas.geojson/+server.ts
@@ -1,8 +1,7 @@
 // import { authorize, readFile } from '$lib/utils';
-import { authorize } from '$lib/utils';
 
 export async function GET({ params, request }) {
-  authorize(request.headers, params.slug);
+  // authorize(request.headers, params.slug);
   // const stream = await readFile(`${params.slug}/areas.geojson`);
   // return new Response(stream);
   return new Response('test');
